2e109f722c16afa95c8ce5e931fbb9e66792c0ac
federico cocco <fcocco01@mail.bbk.ac.uk.com>
2018-04-01 17:24:25 +0100

    Fixed dendrogram drawer object

--- src/main/scala/org/Fcocco01/DocumentClassifier/HierarchicalGraphic.scala
+++ src/main/scala/org/Fcocco01/DocumentClassifier/HierarchicalGraphic.scala
@@ -3,6 +3,7 @@ package org.Fcocco01.DocumentClassifier
 import scalafx.Includes._
 import java.awt.Toolkit
 
+import javafx.scene.transform.Rotate
 import org.Fcocco01.DocumentClassifier.Clustering.HierarchicalClustering.Cluster
 import scalafx.geometry.Pos
 import scalafx.scene._
@@ -28,9 +29,7 @@ object HierarchicalGraphic {
         val center = new StackPane
         root.setContent(center)
 
-        val drawer = new DendrogramDrawer(cluster, center,(w, h))
-        drawer.draw
-
+        DrawDendrogram(cluster, center,(w, h))
         root.setFitToHeight(true);
         val newP = new Tree(cluster)(root, w, h)
         stage.setOnCloseRequest((t: WindowEvent) => println("Close"))
@@ -39,98 +38,82 @@ object HierarchicalGraphic {
       }
     }
 
-  class DendrogramDrawer(cluster: Cluster, n: Pane, measures: (Double,Double)) {
-    def draw = {
-      val anchorpane = new AnchorPane
-      anchorpane.setMaxSize(1000000000,1000000000)
-//      anchorpane.autosize
-      val (newW,newH) = (measures._1 * 0.7, measures._2 * 0.5)
-      anchorpane.setMinSize(measures._1,measures._2)
-      anchorpane.setPrefSize(measures._1,measures._2)
+  object DrawDendrogram {
+    def apply(cluster: Cluster, n: Pane, measures: (Double, Double)): Unit = {
 
-      drawTree(anchorpane, cluster,measures._1 ,measures._2)
-//      n.setContent(anchorpane)
-      n.autosize
-      n.children.add(anchorpane)
-    }
-
-    def drawTree(n: AnchorPane, node: Cluster, width : Double, height: Double) = {
-      val h = node.getHeight + height
+      val (width, height) = measures
+      val h = cluster.getHeight + height
       val w = width
-      val depth = getDepth(node)
-      val scaling = (w-1500)/depth
-      var it = 1
-      println("")
-      println(s"Iteration ${it -1}: For node ${node.hashCode} the x is ${10} and y is ${h/2} and ll is ${node.distance.getOrElse(0.0) * scaling}")
-      def drawnode(node : Cluster, x : Double, y : Int): Unit = {
+      val depth = getDepth(cluster)
+      val scaling = (w - 500) / depth
+
+      val anchorpane = new AnchorPane
+      AnchorPane.setTopAnchor(anchorpane, 100.0)
+      AnchorPane.setLeftAnchor(anchorpane, 10.0)
+      AnchorPane.setRightAnchor(anchorpane, 10.0)
+      AnchorPane.setBottomAnchor(anchorpane, 10.0)
+
+      //      anchorpane.setMaxSize(1000000000,1000000000)
+      //      anchorpane.setMinSize(measures._1,measures._2)
+      //      anchorpane.setPrefSize(measures._1,measures._2
+      //      val (newW,newH) = (measures._1 * 0.7, measures._2 * 0.5)
+
+
+      var it = 0
+
+      def drawNode(node: Cluster, x: Double, y: Double): Unit = {
         it += 1
-        println(s"Iteration ${it -1}: For node ${node.hashCode} the x is ${x} and y is ${y} and ll is ${node.distance.getOrElse(0.0) * scaling}")
-        if(node.isLeaf) {
-          n.children.add({
-//            val t = new Text(x+5,y-7,node.name)
+        println(s"Iteration ${it - 1}: For node ${node.hashCode} the x is ${x} and y is ${y} and ll is ${node.distance.getOrElse(0.0) * scaling}")
+        if (node.isLeaf) {
+          anchorpane.children.add({
+            //            val t = new Text(x+5,y-7,node.name)
             println(s"It is a leaf for ${node.name}")
             println("")
-            val c = Circle(x,y, 5,Color.Red)
+            val c = Circle(x, y, 3.5, Color.Red)
             c.onMouseClicked = (event: MouseEvent) => new Alert(AlertType.Information, node.name).showAndWait
-            c})
+            c
+          })
         }
         else {
           node.getChildren match {
             case None => {}
-            case Some((left,right)) => {
+            case Some((left, right)) => {
               println(s"Children are ${left.hashCode} and ${right.hashCode}")
               println("")
-              val (h1,h2) = (left.getHeight * 20,right.getHeight * 20)
-              val (top,bottom) = (y-(h1+h2)/2,y+(h1+h2)/2)
-              val ll = node.distance.getOrElse(0.0) * (scaling) + 10
+              val (h1, h2) = (left.getHeight * 20, right.getHeight * 20)
+              val (top, bottom) = (y - (h1 + h2) / 2, y + (h1 + h2) / 2)
+              val ll = node.distance.getOrElse(0.0) * scaling + 10
               val merge = {
-                val c = Circle(x, y, 3,Color.BlueViolet)
-//                val s = s"${node.name}\nVectors in the clusters:\n${node.vectors.map(x => s"${x.id}\n")}"
+                val c = Circle(x, y, 3.5, Color.BlueViolet)
                 val s = node.hashCode.toString
                 c.onMouseClicked = (event: MouseEvent) => new Alert(AlertType.Information, s).showAndWait
-                c}
-//              val nextX = (b: Cluster) => b.isLeaf match {case true => (deepest * scaling) + x; case false => x+ll}
-//              val nextX = (b:Cluster) => x+ll
-              n.children.addAll(merge, Polyline(
-                                  x, top+h1/2, x+ll, top+h1/2,
-                                  x, top+h1/2, x, bottom-h2/2, // Central connecting line
-                                  x, bottom-h2/2, x+ll, bottom-h2/2))
-              drawnode(left, x+ll, (top+h1/2).toInt)
-              drawnode(right, x+ll, (bottom-h2/2).toInt)
+                c
+              }
+              anchorpane.children.addAll(merge, Polyline(
+                x, top + h1 / 2, x + ll, top + h1 / 2,
+                x, top + h1 / 2, x, bottom - h2 / 2, // Central connecting line
+                x, bottom - h2 / 2, x + ll, bottom - h2 / 2))
+              drawNode(left, x + ll, top + h1 / 2)
+              drawNode(right, x + ll, bottom - h2 / 2)
             }
           }
         }
       }
-      drawnode(node, 10, (h/2).toInt)
+
+      drawNode(cluster, 10, h / 2)
+
+      //      n.setMinHeight(anchorpane.minHeight.value)
+      //      n.setMaxWidth(anchorpane.minWidth.value)
+      n.children.add(anchorpane)
+      //      n.transforms.add(new Rotate(90, Rotate.X_AXIS))
     }
 
-    def getDepth(c : Cluster) : Double = {
+
+    def getDepth(c: Cluster): Double =
       c.getChildren match {
         case None => 0
-        case Some((left,right)) => Math.max(getDepth(left),getDepth(right)) + c.distance.getOrElse(0.0)
+        case Some((left, right)) => Math.max(getDepth(left), getDepth(right)) + c.distance.getOrElse(0.0)
       }
-    }
-
-    def getDeepestDistance(c: Cluster) = {
-      c.getChildren match {
-        case None => c.distance.getOrElse(0.0)
-        case Some((left,right)) => Math.max(left.distance.getOrElse(0.0),right.distance.getOrElse(0.0))
-      }
-    }
   }
 
-  class Info(a: AlertType, s : String) extends Dialog{
-
-  }
-
-  class Focus (c: Cluster, w: Double,h: Double) {
-    val stage = new Stage
-    val scene = new Scene
-    val scroll = new ScrollPane
-    val anchorPane = new AnchorPane
-    anchorPane.setMaxSize(1000000000,1000000000)
-    anchorPane.setMinSize(w,h)
-    anchorPane.setPrefSize(w,h)
-    //stage.setScene()
-  }
 }
