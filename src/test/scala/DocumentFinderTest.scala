ee198a56eba3f12ffa105d96808b640887c61c9f
federico cocco <federico.cocco@gmail.com>
2018-05-07 00:41:27 +0100

    Changed tests names

--- src/test/scala/DocumentFinderTest.scala
+++ src/test/scala/DocumentFinderTest.scala
@@ -11,19 +11,19 @@ class DocumentFinderTest extends UnitTest("Core.DocGathering.DocumentFinder") {
 
   val testPaths = Array(testDirPath1,testDirPath2,testDirPath3)
 
-  it should "Return a List of paths" in {
+  it should "return a List of paths" in {
     val paths = DocumentFinder.apply(testPaths)
     paths.isInstanceOf[List[DocPath]] shouldBe true
     paths.size shouldBe 7
   }
 
-  it should "Return only expected types of files" in {
+  it should "return only expected types of files" in {
     val paths = DocumentFinder(Array(testDirPath3),Array(new File(testDirPath4).getCanonicalPath))
     assert(paths.forall(x => x.path.contains(".pdf") || x.path.contains(".doc") || x.path.contains(".docx") ||
       x.path.contains(".txt") || x.path.contains(".log")))
   }
 
-  it should "Return paths" in {
+  it should "return the expected number of paths" in {
     val paths = DocumentFinder(Array(testDirPath3),Array(new File(testDirPath4).getCanonicalPath))
     paths.length shouldBe 1
   }
